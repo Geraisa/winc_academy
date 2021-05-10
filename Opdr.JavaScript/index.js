6df36db015a2d55704f50c50bee600280750658f
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-05-10 14:54:37 +0200

    Add files via upload

---
+++ Opdr.JavaScript/index.js
@@ -0,0 +1,8 @@
+console.log("Hello Winc Academy")
+        let name = "Geraisa";
+        console.log(name);    
+        let age = "35";
+        console.log(age);   
+        let number = (4+4);
+        console.log(number); 
+    
