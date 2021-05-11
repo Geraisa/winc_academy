7d94bbb6587edeb5c926deaa2e72a5039368016c
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-05-11 11:29:59 +0200

    Add files via upload

---
+++ Opdr.if-else-statement/script.js
@@ -0,0 +1,23 @@
+//DEEL 1: code gedraaid in Node en alle verschillende inputs werken.
+const age = 35;
+if (age >= 18) {
+    console.log("Yes, you can enter");
+}else {
+    console.log("Sorry, You cant enter");
+}
+
+//DEEL 2: code gedraaid in Node en alle verschillende inputs werken.
+const isFemale = (false);
+if (isFemale) {
+    console.log("Its time for Ladiesnight!");
+} else {
+    console.log("Sorry, only ladies can enter");
+}
+
+//DEEL 2: code gedraaid in Node en alle verschillende inputs werken.
+const driveStatus = "bob";
+if (driveStatus == "bob") {
+    console.log("You are save to drive");
+}else if (driveStatus != "bob") {
+    console.log("You have been drinking, you cant drive!")
+}
