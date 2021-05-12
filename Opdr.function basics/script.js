5f16591e63b637ba29a3cd42f9e589ab40264699
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-05-12 18:05:55 +0200

    Add files via upload

---
+++ Opdr.function basics/script.js
@@ -0,0 +1,33 @@
+/*const makingPizza = function(){
+    console.log('Choose what you want on your pizza');
+    console.log('Make the dough');
+    console.log('Make the saus');
+};
+
+makingPizza();
+makingPizza();
+makingPizza();
+
+const makingSushi = function(){
+    console.log('Choose what sushi');
+    console.log('Choose a saus');
+};
+
+makingSushi();
+makingSushi();
+makingSushi();
+*/
+
+const orderPizza = function(){
+    console.log('a pizza order');
+};
+
+const orderSushi = function(){
+    console.log('a Sushi order');
+};
+
+orderSushi();
+orderSushi();
+orderSushi();
+orderPizza();
+orderPizza();
