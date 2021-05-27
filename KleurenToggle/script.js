0d78a5cf5f69df7b7fc21a90f2639d5f411ebb2e
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-05-27 12:20:19 +0200

    Add files via upload

---
+++ KleurenToggle/script.js
@@ -0,0 +1,34 @@
+const home = document.querySelector(".homeColor")
+const red = document.querySelector(".redColor")
+const yellow = document.querySelector(".yellowColor")
+const green = document.querySelector(".greenColor")
+const blue = document.querySelector(".blueColor")
+const purple = document.querySelector(".purpleColor")
+
+home.addEventListener('click', e => {
+    document.body.style.backgroundColor = "white";
+})
+
+red.addEventListener('click', e => {
+    document.body.style.backgroundColor = "red";
+})
+
+yellow.addEventListener('click', e => {
+    document.body.style.backgroundColor = "yellow";
+})
+
+green.addEventListener('click', e => {
+    document.body.style.backgroundColor = "green";
+})
+
+blue.addEventListener('click', e => {
+    document.body.style.backgroundColor = "blue";
+})
+
+purple.addEventListener('click', e => {
+    document.body.style.backgroundColor = "purple";
+})
+
+$('.navbar-collapse a').click(function(){
+    $(".navbar-collapse").collapse('hide');
+});
