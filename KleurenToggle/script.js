df90674bc4659ba79c2e2b104462097022f4d76a
Geraisa <83660951+Geraisa@users.noreply.github.com>
2021-05-31 19:50:49 +0200

    Update script.js

--- KleurenToggle/script.js
+++ KleurenToggle/script.js
@@ -4,27 +4,46 @@ const yellow = document.querySelector(".yellowColor")
 const green = document.querySelector(".greenColor")
 const blue = document.querySelector(".blueColor")
 const purple = document.querySelector(".purpleColor")
+const menu = document.querySelector(".menu")
+const hamburger = document.querySelector(".hamburger")
+
+
+hamburger.addEventListener('click', e => {
+    menu.classList.toggle('menu')
+})
+
 
 home.addEventListener('click', e => {
     document.body.style.backgroundColor = "white";
+    menu.classList.toggle('menu')
 })
 
 red.addEventListener('click', e => {
     document.body.style.backgroundColor = "red";
+    menu.classList.toggle('menu')
 })
 
 yellow.addEventListener('click', e => {
     document.body.style.backgroundColor = "yellow";
+    menu.classList.toggle('menu')
 })
 
 green.addEventListener('click', e => {
     document.body.style.backgroundColor = "green";
+    menu.classList.toggle('menu')
 })
 
 blue.addEventListener('click', e => {
     document.body.style.backgroundColor = "blue";
+    menu.classList.toggle('menu')
 })
 
 purple.addEventListener('click', e => {
     document.body.style.backgroundColor = "purple";
+    menu.classList.toggle('menu')
 })
+
+hamburger.addEventListener("mouseover", e => {
+    menu.classList.toggle("visible");
+})
+
